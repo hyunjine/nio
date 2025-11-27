805ab6acc84f4ac93fe6433118f67f6cb5b1789f
hyunjine <thevlakk1@gmail.com>
2025-11-28 02:35:42 +0900

    feat: dday 기능 구현 완료

--- feature/d-day/src/main/java/com/hyunjine/d_day/widget/CounterWidget.kt
+++ feature/d-day/src/main/java/com/hyunjine/d_day/widget/CounterWidget.kt
@@ -4,12 +4,17 @@ import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Column
@@ -20,27 +25,29 @@ import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 import com.hyunjine.common.log.wlog
+import com.hyunjine.d_day.DDayRepository
 import dagger.hilt.android.EntryPointAccessors
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class CounterWidget: GlanceAppWidget() {
+    private val getRepository: GetRepository by lazy {
+        GetRepository()
+    }
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-        val repository = EntryPointAccessors.fromApplication(
-            context.applicationContext,
-            WidgetEntryPoint::class.java
-        ).repository
+        getRepository(context).updateStartDate(LocalDate.of(2025, 3, 8))
 
         provideContent {
+            var dDay by remember { mutableStateOf("") }
             LaunchedEffect(Unit) {
-                repository.getStartDate().collect {
-                    wlog(it)
+                getRepository(context).getStartDate().collect { startDate ->
+                    dDay = ChronoUnit.DAYS.between(startDate, LocalDate.now()).plus(1).toString()
                 }
             }
-            Column {
-                Widget(LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")))
-            }
+            Widget("D+$dDay")
         }
     }
 }
@@ -53,7 +60,8 @@ fun Widget(text: String) {
     Text(
         text = text,
         modifier = GlanceModifier
-            .background(Color.Red)
+            .background(Color.Gray)
+            .cornerRadius(12.dp)
             .padding(20.dp),
         style = TextStyle(
             color = ColorProvider(Color.Black),
@@ -61,3 +69,4 @@ fun Widget(text: String) {
         ),
     )
 }
+
--- feature/d-day/src/main/java/com/hyunjine/d_day/widget/WidgetEntryPoint.kt
+++ feature/d-day/src/main/java/com/hyunjine/d_day/widget/WidgetEntryPoint.kt
@@ -1,12 +1,28 @@
 package com.hyunjine.d_day.widget
 
+import android.content.Context
 import com.hyunjine.d_day.DDayRepository
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
 interface WidgetEntryPoint {
     val repository: DDayRepository
+}
+
+class GetRepository {
+    private lateinit var _repository: DDayRepository
+
+    operator fun invoke(context: Context): DDayRepository {
+        if (!::_repository.isInitialized) {
+            _repository = EntryPointAccessors.fromApplication(
+                context.applicationContext,
+                WidgetEntryPoint::class.java
+            ).repository
+        }
+        return _repository
+    }
 }
